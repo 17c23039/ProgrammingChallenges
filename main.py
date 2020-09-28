9829be9e776ad6f0e8bf817fcaed0c47724213fc
Liam Dixon <17c23039@marling.school>
2020-09-28 11:22:13 +0000

    Basic prototype of ShoppingGuru extension.

---
+++ main.py
@@ -0,0 +1,20 @@
+from time import sleep   #Please note this is only a prototype and will be improved and perfected over time.
+
+print("Hi! I am ShoppingGuru and I will create a shopping list for you. If you would like to add anything after, please just say Y to the question.")
+sl = str(input("What do you want to add to your shopping list? ")) #Asks for shoppuing list.
+print("Thanks. Adding..")
+slanswer = input("Would you like to add any more item(s) to your shopping list? Y/N/END. Please write the answer as we did. Y or N or END. ")
+if slanswer == "N" :
+  print("Okay, your shopping list consists of these items so far:")
+  print(sl)
+elif slanswer == "Y" :
+  sladd1 = str(input("Okay, please make sure you write everything down so we can give you your shopping list. "))
+  print("Thanks, adding and printing your shopping list..")
+  sleep(2.5)
+  print("Okay, your shopping list consists of " + sl + " along with your add ons of " + sladd1)
+  sleep(3)
+  print("Thanks! Use our service again soon!")    
+elif slanswer == "END" :
+  print("Okay, shutting off...")
+else :
+  print("That is an invalid option! Re-run the code to use me again.")
